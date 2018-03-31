82a60dfd0958ce2d12baae49863ed75ba9ca435d
Nicholas Pretorius <nicholaspretorius@gmail.com>
2018-03-31 14:43:17 +0200

    Added debug, eslint and basic register route and template.

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 // GET /
-router.get('/', function(req, res, next) {
-  return res.render('index', { title: 'Home' });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Home' });
 });
 
 // GET /about
-router.get('/about', function(req, res, next) {
-  return res.render('about', { title: 'About' });
+router.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
 });
 
 // GET /contact
-router.get('/contact', function(req, res, next) {
-  return res.render('contact', { title: 'Contact' });
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact' });
 });
+
+// GET /register
+router.route('/register')
+  .get((req, res) => {
+    res.render('register', { title: 'Register' });
+  })
+  .post((req, res) => {
+    res.render('thankyou', { title: 'Thank you' });
+  });
+
 
 module.exports = router;
